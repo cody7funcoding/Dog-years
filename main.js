630beafed889b77b8b849521a0574836757acb46
Cody <codygmcneil@gmail.com>
2022-02-21 17:32:25 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+/ I'm setting my age to a variable
+const myAge = 34;
+// I'm setting up a variable to represent a dog's early years.
+let earlyYears = 2;
+earlyYears *= 10.5
+//I'm taking my age and subtracting 2, saving to a variable.
+let laterYears = myAge - 2; 
+// This is the number of dog years for my later years. 
+laterYears *= 4;
+//I'm adding my early years plus later years.
+let myAgeInDogYears = earlyYears + laterYears;
+//This is a string to convert to lower case.
+let myName ='Cody McNeil'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
